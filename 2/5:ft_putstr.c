8da39d6014d8df79d39c07bf928f932cf4acebe7
jxraf <151397673+jxraf@users.noreply.github.com>
2024-12-24 11:51:40 +0000

    creat 5;ft_putstr

---
+++ 2/5:ft_putstr.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+void ft_putstr (char *str) {
+ int s = 0;
+
+ while (str[s] != '\0') {
+    write (1,&str[s],1) ;
+    s++;
+ } 
+}
+int main (void) {
+ft_putstr("nigga snake");
+    return 0;
+}
