8ab388f3c71e73b9a9b7b62706cfd518de608d9f
jxraf <151397673+jxraf@users.noreply.github.com>
2025-01-23 12:03:34 +0100

    Update 4:ft_atoi.c

--- 5/4:ft_atoi.c
+++ 5/4:ft_atoi.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <stdio.h>                                 
 int ft_atoi(char *str) {
     int minus = 0;
     int num = 0;
@@ -17,4 +17,26 @@ if (!(minus % 2)) {
     return (num);
 }
     return (-num); 
+ }
+or
+#include <stdio.h>
+int ft_atoi(char *str) {
+   int minus = 0;
+   int num = 0;
+   while (*str) {
+       if ((*str >= 9 && *str <= 13) || (*str == 32 )) 
+       { str++; }
+        else if (*str == '+' || *str == '-') { 
+            if (*str == '-') { ++minus;}
+            ++str; }
+      else if (*str >= 48 && *str <= 57) {
+           num *= 10;
+           num += *str - 48;
+           ++str;
+       }
+       else { break; }
+   }
+       if (minus % 2) {
+           return (-num);
+       } return (num);
  }
