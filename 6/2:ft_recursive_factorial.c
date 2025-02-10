b9a8a480833d07a010df97f994c81a474f8265c6
jxraf <151397673+jxraf@users.noreply.github.com>
2025-02-10 12:33:51 +0100

    2:ft_recursive_factorial.c

---
+++ 6/2:ft_recursive_factorial.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int ft_recursive_factorial (int nb) {
+ if  (nb == 0 || nb == 1 ) { return 1;}
+    if (nb < 0) {return 0;}
+    else 
+    return nb * ft_recursive_factorial(nb - 1);
+}
+
