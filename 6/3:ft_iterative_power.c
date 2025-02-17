2fcc7d651b08aa356e14dd58d65a501777706d9d
jxraf <151397673+jxraf@users.noreply.github.com>
2025-02-17 12:24:34 +0100

    Create 3:ft_iterative_power.c

---
+++ 6/3:ft_iterative_power.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int ft_iterative_power(int nb, int power) {
+
+int res = nb;
+while (power > 1) {
+    res *= nb;
+    --power;
+    return res;
+}
+    if (power < 0) {return 0;}
+    if ( power == 0 && nb == 0) {return 1;}
+    if ( power == 0) {return 1 ;} 
+}
