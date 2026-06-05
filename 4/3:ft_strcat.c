484a53ab95ccecdeed19e6769c133b36b503f4f2
jxraf <151397673+jxraf@users.noreply.github.com>
2026-06-05 18:57:05 +0100

    Update 3:ft_strcat.c

--- 4/3:ft_strcat.c
+++ 4/3:ft_strcat.c
@@ -6,10 +6,9 @@ while (dest[x] != '\0') {
     x++;
 }
 while (src[y] != '\0') {
-    dest[x] = src[y];
-    x++;
+    dest[x + y] = src[y];
     y++;
 }
-dest[x] ='\0';
+dest[x + y] ='\0';
 return dest;
 }
