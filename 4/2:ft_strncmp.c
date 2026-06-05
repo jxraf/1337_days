721d19df2ae95c68662daecf3cd47c74377473d4
jxraf <151397673+jxraf@users.noreply.github.com>
2026-06-05 18:11:20 +0100

    Update 2:ft_strncmp.c

--- 4/2:ft_strncmp.c
+++ 4/2:ft_strncmp.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
-int ft_strncmp(char *s1, char *s2, unsigned int n){
-    while (*s1 == *s2 && *s1 && n--) {
-        s1++, s2++;
-    }
-    return (*s1 - *s2);   
+int ft_strcmp(char *s1, char *s2, unsigned int n) {
+    int i;
+    i = 0;
+
+    while(n-- &&(s1[i] || s2[i])
+    {
+        if (s1[i] != s2[i]) 
+            return(s1[i] - s2[2]);
+        i++;
+    }  
+    return 0;
 }
