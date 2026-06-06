50cacc23f3d341431d72b5fc4f8e1495e82605b8
jxraf <151397673+jxraf@users.noreply.github.com>
2026-06-06 15:33:57 +0100

    Update 2:ft_putstr.c

--- 5/2:ft_putstr.c
+++ 5/2:ft_putstr.c
@@ -1,7 +1,11 @@
 #include <unistd.c>
-void ft_putstr(char *str) {
-  int s=0; 
-while (str[s] !='\0') {
-write (1,&str[s],1);
-s++; }
+void ft_putstr(char *str) 
+{
+  int s;
+  s = 0; 
+  while (str[s]) 
+  {
+      write (1, &str[s], 1);
+      s++; 
+  }
 }
