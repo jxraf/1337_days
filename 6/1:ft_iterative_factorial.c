92f7d984fba0fef7ebac8921ff6a9bc4e6f89405
jxraf <151397673+jxraf@users.noreply.github.com>
2026-06-08 12:48:10 +0000

    update project

--- 6/1:ft_iterative_factorial.c
+++ 6/1:ft_iterative_factorial.c
@@ -2,15 +2,13 @@
 
 int ft_iterative_factorial(int nb) {
 
-    if  (nb == 0 || nb == 1 ) { return 1;}
-    if (nb < 0) {return 0;}
-    
-    int nbr = 1;
-    int result = 1;
-    while (nbr < nb) {
-        nbr++;
-        result *= nbr;
-    }  
-        return result;
- 
+    int i;
+    i = 0;
+
+    while(n > 1)
+    {
+        i *= nb;
+        n--;
+    }
+ return i;
 }
--- testing.c
+++ testing.c
@@ -1,93 +1,20 @@
-#include <unistd.h>
 #include <stdio.h>
 
-void ft_putchar(char c) 
+int ft_fact(int nb)
 {
-        write(1, &c, 1);
+        int i;
+        i = 1;
+     while(nb < 1)
+     {
+        i *= nb; 
+        nb--;
+     }
+     return i;
 }
 
-void ft_cmb2(void)
+int main()
 {
-        char a;
-        char b;
-        char c;
-        char d;
+   ft_fact(5);
 
-        a = '0';
-        while (a <= 9)
-        {
-                b = '0';
-                while (b <= 8)
-                {
-                        c = a + 1;
-                        while (c <= 9)
-                        {
-                                d = b + 1;
-                                while (d <= 9)
-                                {
-                                        ft_putchar(a);
-                                        ft_putchar(b);
-                                        ft_putchar(' ');
-                                        ft_putchar(c);
-                                        ft_putchar(d);
-                                        
-                                        d++;
-                                }c++;
-                        }b++;
-                }a++;
-        }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// void ft_comb(void)
-// {
-//         char a;
-//         char b;
-//         char c;
-
-//         a = '0';
-
-//         while (a <= '7')
-//         {
-//                 b = a + 1;
-//                 while (b <= '8')
-//                 {
-//                         c = b + 1;
-//                         while (c <= '9')
-//                         {
-//                                 ft_putchar(a);
-//                                 ft_putchar(b);
-//                                 ft_putchar(c);
-//                                 if (a != '7' || b != '8' || c != '9') 
-//                                 write(1, ", ", 2);
-                        
-//                         c++;
-//                         }
-                       
-//                         b++;
-//                 }
-                
-//                 a++;
-//         }
-// }
-
-
-int main ()
-{
-        ft_cmb2();
-
-        return 0;
+   return 0;
 }
