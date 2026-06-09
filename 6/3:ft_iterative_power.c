60de4a08baf4e4eb5950ffa518279c81f38e883e
jxraf <151397673+jxraf@users.noreply.github.com>
2026-06-09 16:09:09 +0000

    updated project

--- 6/3:ft_iterative_power.c
+++ 6/3:ft_iterative_power.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 
-int ft_iterative_power(int nb, int power) {
+int ft_iterative_power(int nb, int power)
+{
+int result;
+result = 1;
 
-    if (power < 0) {return 0;}
-    if ( power == 0) {return 1 ;} 
-    
-int res = 1;
-while (power > 1) {
-    res *= nb;
-    --power;
-}
-    return res;
+   if(power < 0)
+      return 0;
+   
+   while(power > 0)
+   {
+      result *= nb;
+      power--;
+   }   
+   return result;
 }
--- testing.c
+++ testing.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 
-int ft_fact(int nb)
+int ft_iterative_power(int nb, int power)
 {
-     if (nb < 0)
-     return (0);
+int result;
+result = 1;
 
-      if (nb == 0 || nb == 1)
-      {
-         return (1);
-      }
-      else
-      {
-         return nb *= ft_fact(nb - 1);              
-      }
+   if(power < 0)
+      return 0;
+   
+   while(power > 0)
+   {
+      result *= nb;
+      power--;
+   }   
+   return result;
 }
+
 int main()
 {
-   printf("%d",ft_fact(3));
-
-   return 0;
+printf("%d",ft_iterative_power(6,0));
 }
