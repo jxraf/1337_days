c495f903286a60cb676115234aeb272bb4f643be
jxraf <151397673+jxraf@users.noreply.github.com>
2026-06-09 14:48:16 +0000

    updated project

--- testing.c
+++ testing.c
@@ -2,16 +2,21 @@
 
 int ft_fact(int nb)
 {
-        int i;
-        i = 1;
-    
-        
-     return i;
-}
+     if (nb < 0)
+     return (0);
 
+      if (nb == 0 || nb == 1)
+      {
+         return (1);
+      }
+      else
+      {
+         return nb *= ft_fact(nb - 1);              
+      }
+}
 int main()
 {
-   ft_fact(5);
+   printf("%d",ft_fact(3));
 
    return 0;
 }
