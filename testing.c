9d7c1052e2fb9bebe9d132db32fc5e0518620486
jxraf <151397673+jxraf@users.noreply.github.com>
2026-06-09 20:24:18 +0000

    updated

--- testing.c
+++ testing.c
@@ -1,22 +1,25 @@
 #include <stdio.h>
 
-int ft_iterative_power(int nb, int power)
+int ft_recursive_power(int nb, int power)
 {
-int result;
-result = 1;
 
    if(power < 0)
-      return 0;
-   
-   while(power > 0)
    {
-      result *= nb;
-      power--;
-   }   
-   return result;
+      return 0;
+   }
+   
+   if (power == 0)
+   {
+      return 1;
+   }
+   else
+   {
+      return   ft_recursive_power(nb,power - 1) * nb  ;
+   }
 }
-
 int main()
 {
-printf("%d",ft_iterative_power(6,0));
+printf("%d",ft_recursive_power(6,3));
+
+return 0;
 }
