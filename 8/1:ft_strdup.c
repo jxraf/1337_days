2c6aba8448dbf5aa7b49b7db80ac6477b6f22881
jxraf <151397673+jxraf@users.noreply.github.com>
2026-06-19 00:42:38 +0100

    Update 1:ft_strdup.c

--- 8/1:ft_strdup.c
+++ 8/1:ft_strdup.c
@@ -1,1 +1,13 @@
+#include <stdlib.h>
 
+char *ft_strdup(char *src)
+{
+    char *dest;
+    dest = malloc(ft_strlen(src + 1));
+
+    if (dest == NULL)
+        return NULL;
+ 
+    ft_strcpy(dest, src);
+    return dest;
+}
