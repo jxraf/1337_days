74beceb3dba202e3d05d28645b0e9ba7ee413169
jxraf <151397673+jxraf@users.noreply.github.com>
2026-06-30 19:11:23 +0100

    Update 7:ft_sort_int_tab.c

--- 2/7:ft_sort_int_tab.c
+++ 2/7:ft_sort_int_tab.c
@@ -1,11 +1,21 @@
-void ft_sort_int_tab(int *tab, int size) {
-    int y = 0;
-    int z ;
-    
-    while (y < (size / 2)) {
-    z = tab [y];
-    tab [y] = tab[size-1-y];
-    tab[size-1-y]=z;
-     y++;   
+void ft_sort_int_tab(int *tab, int size)
+{
+    int x;
+    int y;
+    int z;
+
+    y = 0;
+    while (x < size)
+    {
+     z = y + 1;
+     while (z < size)
+     {
+         x = tab[y];
+         tab[y] = tab[z];
+         tab[z] = x;
+         z++;
+     }
+     y++;
     }
-                                                                /*unfinished*/
+}
+                                                       
